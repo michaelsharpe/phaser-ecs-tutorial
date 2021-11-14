88c27aa29fc7395232a1b95d385a80361269da6a
Michael Sharpe <m.sharpe@project89.org>
2021-11-14 12:26:44 -0800

    Add game scene with a few components

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,23 @@
 import Phaser from "phaser";
 
+import {
+  addComponent,
+  addEntity,
+  createWorld,
+  defineComponent,
+  Types,
+} from "bitecs";
+
+const Position = defineComponent({
+  x: Types.f32,
+  y: Types.f32,
+});
+
+const Velocity = defineComponent({
+  x: Types.f32,
+  y: Types.f32,
+});
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super("game");
@@ -12,6 +30,23 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
+    const world = createWorld();
+
+    // tank is basically just an id
+    const tank = addEntity(world);
+
+    // Added position component to tank entity
+    addComponent(world, Position, tank);
+
+    // component properties are an array that stores the properties of an entity
+    Position.x[tank] = 100;
+    Position.y[tank] = 100;
+
+    addComponent(world, Velocity, tank);
+
+    Velocity.x[tank] = 5;
+    Velocity.y[tank] = 5;
+
     // CREATE ENTITIES
     // ATTACH COMPONENTS
     // CREATE SYSTEMS
