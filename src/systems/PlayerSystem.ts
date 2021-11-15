de7a30c28b51dbe196c951c5521ccdde3824f9a4
Michael Sharpe <m.sharpe@project89.org>
2021-11-14 23:25:20 -0800

    Set rotation in player system

--- src/systems/PlayerSystem.ts
+++ src/systems/PlayerSystem.ts
@@ -1,11 +1,12 @@
 import { defineQuery, defineSystem } from "bitecs";
 import { Velocity } from "../components/Velocity";
 import { Player } from "../components/Player";
+import { Rotation } from "../components/Rotation";
 
 export const createPlayerSystem = (
   cursors: Phaser.Types.Input.Keyboard.CursorKeys
 ) => {
-  const query = defineQuery([Player, Velocity]);
+  const query = defineQuery([Player, Velocity, Rotation]);
 
   return defineSystem((world) => {
     const entities = query(world);
@@ -16,15 +17,19 @@ export const createPlayerSystem = (
       if (cursors.left.isDown) {
         Velocity.x[id] = -5;
         Velocity.y[id] = 0;
+        Rotation.angle[id] = 180;
       } else if (cursors.right.isDown) {
         Velocity.x[id] = 5;
         Velocity.y[id] = 0;
+        Rotation.angle[id] = 0;
       } else if (cursors.up.isDown) {
         Velocity.x[id] = 0;
         Velocity.y[id] = -5;
+        Rotation.angle[id] = 270;
       } else if (cursors.down.isDown) {
         Velocity.x[id] = 0;
         Velocity.y[id] = 5;
+        Rotation.angle[id] = 90;
       } else {
         Velocity.x[id] = 0;
         Velocity.y[id] = 0;
