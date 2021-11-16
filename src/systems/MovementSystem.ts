0abde1fde106b6f6ca582d4724749a231e36e9ed
Michael Sharpe <m.sharpe@project89.org>
2021-11-15 22:45:03 -0800

    Handle directions in movement system

--- src/systems/MovementSystem.ts
+++ src/systems/MovementSystem.ts
@@ -1,15 +1,47 @@
 import { defineQuery, defineSystem } from "bitecs";
 import { Velocity } from "../components/Velocity";
 import { Position } from "../components/Position";
+import { Input, Direction } from "../components/Input";
+import { Rotation } from "../components/Rotation";
 
 export const createMovementSystem = () => {
-  const query = defineQuery([Position, Velocity]);
+  const query = defineQuery([Position, Velocity, Input, Rotation]);
+  const speed = 5;
 
   return defineSystem((world) => {
     const entities = query(world);
 
     for (let i = 0; i < entities.length; i++) {
       const id = entities[i];
+
+      const direction = Input.direction[id];
+
+      switch (direction) {
+        case Direction.None:
+          Velocity.x[id] = 0;
+          Velocity.y[id] = 0;
+          break;
+        case Direction.Down:
+          Velocity.x[id] = 0;
+          Velocity.y[id] = speed;
+          Rotation.angle[id] = 90;
+        case Direction.Up:
+          Velocity.x[id] = 0;
+          Velocity.y[id] = -speed;
+          Rotation.angle[id] = 270;
+          break;
+        case Direction.Right:
+          Velocity.x[id] = speed;
+          Velocity.y[id] = 0;
+          Rotation.angle[id] = 0;
+          break;
+        case Direction.Left:
+          Velocity.x[id] = -speed;
+          Velocity.y[id] = 0;
+          Rotation.angle[id] = 180;
+          break;
+      }
+
       Position.x[id] += Velocity.x[id];
       Position.y[id] += Velocity.y[id];
     }
