3489d3454e2ba298a5c76a1417724b4f156da671
Michael Sharpe <m.sharpe@project89.org>
2021-11-15 22:45:31 -0800

    Use direction component in CPU system

--- src/systems/CPUSystem.ts
+++ src/systems/CPUSystem.ts
@@ -3,9 +3,10 @@ import { defineQuery, defineSystem } from "bitecs";
 import { Rotation } from "../components/Rotation";
 import { CPU } from "../components/CPU";
 import { Velocity } from "../components/Velocity";
+import { Direction, Input } from "../components/Input";
 
 export const createCPUSystem = (scene: Phaser.Scene) => {
-  const cpuQuery = defineQuery([CPU, Velocity, Rotation]);
+  const cpuQuery = defineQuery([CPU, Velocity, Rotation, Input]);
 
   return defineSystem((world) => {
     const dt = scene.game.loop.delta;
@@ -25,34 +26,25 @@ export const createCPUSystem = (scene: Phaser.Scene) => {
       switch (rand) {
         // left
         case 0:
-          Velocity.x[id] = -5;
-          Velocity.y[id] = 0;
-          Rotation.angle[id] = 180;
+          Input.direction[id] = Direction.Left;
           break;
 
         // right
         case 1:
-          Velocity.x[id] = 5;
-          Velocity.y[id] = 0;
-          Rotation.angle[id] = 0;
+          Input.direction[id] = Direction.Right;
           break;
 
         // up
         case 2:
-          Velocity.x[id] = 0;
-          Velocity.y[id] = -5;
-          Rotation.angle[id] = 270;
+          Input.direction[id] = Direction.Up;
           break;
 
         // down
         case 3:
-          Velocity.x[id] = 0;
-          Velocity.y[id] = 5;
-          Rotation.angle[id] = 90;
+          Input.direction[id] = Direction.Down;
           break;
         default:
-          Velocity.x[id] = 0;
-          Velocity.y[id] = 0;
+          Input.direction[id] = Direction.None;
       }
     }
 
