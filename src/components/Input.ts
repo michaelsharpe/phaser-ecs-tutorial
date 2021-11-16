005d72a5bcc967bc292df9cf0b5352ee8e45522b
Michael Sharpe <m.sharpe@project89.org>
2021-11-15 22:44:48 -0800

    Add input component

---
+++ src/components/Input.ts
@@ -0,0 +1,13 @@
+import { defineComponent, Types } from "bitecs";
+
+export enum Direction {
+  None,
+  Left,
+  Right,
+  Up,
+  Down,
+}
+
+export const Input = defineComponent({
+  direction: Types.ui8,
+});
